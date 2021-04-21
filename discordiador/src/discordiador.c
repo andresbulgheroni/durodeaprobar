4a4f9036fb1bfbda0740881ff1da868f4529f97b
andresbulgheroni <abulgheroni@est.frba.utn.edu.ar>
2021-04-21 19:08:35 -0300

    agrego archivo de config y lo inicializo

--- discordiador/src/discordiador.c
+++ discordiador/src/discordiador.c
@@ -7,15 +7,53 @@
  Description : Hello World in C, Ansi-style
  ============================================================================
  */
-
+#include "discordiador.h"
 #include <stdio.h>
 #include <stdlib.h>
 
-int main(void) {
 
+
+t_config*config;
+
+
+
+int main(void){
+
+	inicializarConfig(config);
 
 	puts("!!!hola soy el discordiador!!!"); /* prints !!!Hello World!!! */
 	return EXIT_SUCCESS;
+
+}
+
+
+void inicializarConfig(t_config* config){
+
+
+	config= config_create("discord.config");
+	if( config==NULL){
+		printf("no se pudo leer archivo config");
+		exit(2);
+	}
+	IP_MI_RAM_HQ=config_get_string_value(config,"IP_MI_RAM_HQ");
+	PUERTO_MI_RAM_HQ=config_get_string_value(config,"PUERTO_MI_RAM_HQ");
+	IP_I_MONGO_STORE=config_get_string_value(config,"IP_I_MONGO_STORE");
+	ALGORITMO=config_get_string_value(config,"ALGORITMO");
+	PUERTO_I_MONGO_STORE=config_get_string_value(config,"PUERTO_I_MONGO_STORE");
+	GRADO_MULTITAREA=config_get_int_value(config,"GRADO_MULTITAREA");
+	QUANTUM=config_get_int_value(config,"QUANTUM");
+	DURACION_SABOTAJE=config_get_int_value(config,"DURACION_SABOTAJE");
+	RETARDO_CICLO_CPU=config_get_int_value(config,"RETARDO_CICLO_CPU");
+
+	printf("el valor es: %s\n",IP_MI_RAM_HQ);
+	printf("el valor es: %s\n",PUERTO_MI_RAM_HQ);
+	printf("el valor es: %s\n",IP_I_MONGO_STORE);
+	printf("el valor es: %s\n",ALGORITMO);
+	printf("el valor es: %s\n",PUERTO_I_MONGO_STORE);
+	printf("el valor es: %d\n",GRADO_MULTITAREA);
+	printf("el valor es: %d\n",QUANTUM);
+	printf("el valor es: %d\n",DURACION_SABOTAJE);
+	printf("el valor es: %d\n",RETARDO_CICLO_CPU);
 
 
 }
--- discordiador/src/discordiador.h
+++ discordiador/src/discordiador.h
@@ -1,13 +1,31 @@
-/*
- * discordiador.h
- *
- *  Created on: 20 abr. 2021
- *      Author: utnso
- */
-
 #ifndef DISCORDIADOR_H_
 #define DISCORDIADOR_H_
 
+#include<stdio.h>
+#include<stdint.h>
+#include<stdlib.h>
+#include<commons/string.h>
+#include<commons/config.h>
+#include<commons/log.h>
+#include<readline/readline.h>
+#include<commons/collections/list.h>
+#include<commons/collections/queue.h>
+#include<pthread.h>
+#include<semaphore.h>
+
+
+
+char*IP_MI_RAM_HQ;
+char*PUERTO_MI_RAM_HQ;
+char*IP_I_MONGO_STORE;
+char*PUERTO_I_MONGO_STORE;
+int GRADO_MULTITAREA;
+char*ALGORITMO;
+int QUANTUM;
+int DURACION_SABOTAJE;
+int RETARDO_CICLO_CPU;
+
+void inicializarConfig(t_config*);
 
 
 #endif /* DISCORDIADOR_H_ */
