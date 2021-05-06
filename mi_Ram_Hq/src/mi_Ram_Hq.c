303e1dfc798e3d921786641d3e8f33aedd71f1c3
smchejolan <smchejolan@gmail.com>
2021-05-06 00:04:47 -0300

    Lectura de config de RAM

--- mi_Ram_Hq/src/mi_Ram_Hq.c
+++ mi_Ram_Hq/src/mi_Ram_Hq.c
@@ -12,7 +12,54 @@
 #include <stdlib.h>
 #include "mi_Ram_Hq.h"
 
+
+
 int main(void) {
-	puts("!!!soy la ram!!!"); /* prints !!!Hello World!!! */
+
+	init();
+
+	terminar();
+
 	return EXIT_SUCCESS;
+}
+
+void init (){
+
+	config = config_create("ram.config");
+	logger = log_create("ram.log", "MI-RAM-HQ", true, LOG_LEVEL_DEBUG);
+
+	TAMANIO_MEMORIA = config_get_int_value(config, "TAMANIO_MEMORIA");
+	ESQUEMA_MEMORIA = get_esquema_memoria(config_get_string_value(config, "ESQUEMA_MEMORIA"));
+	TAMANIO_PAGINA = config_get_int_value(config, "TAMANIO_PAGINA");
+	TAMANIO_SWAP = config_get_int_value(config, "TAMANIO_SWAP");
+	PATH_SWAP = config_get_string_value(config, "PATH_SWAP");
+	ALGORITMO_REEMPLAZO = config_get_string_value(config, "ALGORITMO_REEMPLAZO");;
+	IP = config_get_string_value(config, "IP");
+	PUERTO = config_get_string_value(config, "PUERTO");
+
+	memoria_principal = malloc(TAMANIO_MEMORIA);
+
+}
+
+void terminar(){
+
+	log_destroy(logger);
+	config_destroy(config);
+	free(memoria_principal);
+
+}
+
+int32_t get_esquema_memoria(char* esquema_config) {
+
+	if(strcmp("PAGINACION", esquema_config) == 0){
+
+		return PAGINACION_VIRTUAL;
+
+	} else if(strcmp("SEGMENTACION", esquema_config) == 0){
+
+		return SEGMENTACION_PURA;
+
+	}
+
+	return -1;
 }
--- mi_Ram_Hq/src/mi_Ram_Hq.h
+++ mi_Ram_Hq/src/mi_Ram_Hq.h
@@ -8,6 +8,33 @@
 #ifndef MI_RAM_HQ_H_
 #define MI_RAM_HQ_H_
 
+#include "utils.h"
+#include <nivel-gui/nivel-gui.h>
+#include <nivel-gui/tad_nivel.h>
+#include <commons/config.h>
+#include <commons/log.h>
 
+typedef enum{
+	SEGMENTACION_PURA = 1,
+	PAGINACION_VIRTUAL = 2
+} esquema;
+
+uint32_t TAMANIO_MEMORIA;
+int32_t ESQUEMA_MEMORIA;
+uint32_t TAMANIO_PAGINA;
+uint32_t TAMANIO_SWAP;
+char* PATH_SWAP;
+char* ALGORITMO_REEMPLAZO;
+char* IP;
+char* PUERTO;
+
+void* memoria_principal;
+
+t_config* config;
+t_log* logger;
+
+void init ();
+void terminar ();
+int32_t get_esquema_memoria(char* esquema_config);
 
 #endif /* MI_RAM_HQ_H_ */
