f608e099f157fb9a4017dbf836c78ff0a45a6666
snouthill <dagostino@frba.utn.edu.ar>
2021-05-18 20:18:39 -0300

    Cambios de Andres para conectar con su modulo

--- i_Mongo_Store/src/i_Mongo_Store.c
+++ i_Mongo_Store/src/i_Mongo_Store.c
@@ -1,13 +1,3 @@
-/*
- ============================================================================
- Name        : i_Mongo_Store.c
- Author      : duroDeAProbar
- Version     :
- Copyright   : Your copyright notice
- Description : Hello World in C, Ansi-style
- ============================================================================
- */
-
 #include <stdio.h>
 #include <stdlib.h>
 #include "i_Mongo_Store.h"
