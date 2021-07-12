6fb3b658b946273115deaa670780f69016784f19
andresbulgheroni <49323532+andresbulgheroni@users.noreply.github.com>
2021-07-12 14:25:19 -0300

    Update mi_Ram_Hq.h

--- mi_Ram_Hq/src/mi_Ram_Hq.h
+++ mi_Ram_Hq/src/mi_Ram_Hq.h
@@ -8,6 +8,228 @@
 #ifndef MI_RAM_HQ_H_
 #define MI_RAM_HQ_H_
 
+#include "utils.h"
+#include <nivel-gui/nivel-gui.h>
+#include <nivel-gui/tad_nivel.h>
+#include <commons/config.h>
+#include <commons/log.h>
+#include <pthread.h>
+#include <curses.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <curses.h>
+#include <commons/collections/list.h>
+#include <commons/collections/dictionary.h>
+#include <commons/temporal.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <sys/mman.h>
+#include <unistd.h>
+#include <netdb.h>
+#include <math.h>
+#include <signal.h>
+#include <time.h>
 
+typedef enum{
+	SEGMENTACION_PURA = 1,
+	PAGINACION_VIRTUAL = 2
+} esquema;
+
+typedef enum{
+	LRU= 1,
+	CLOCK= 2
+} algoritmo_reemplazo;
+
+typedef struct{
+	int nro_pagina;
+	int32_t nro_frame;
+	int nro_frame_mv;
+	bool presente;
+	bool uso;
+	bool modificado;
+	int ocupado;
+
+} t_pagina_patota;
+
+typedef struct{
+
+	t_list* tabla_paginas;
+	pthread_mutex_t m_TABLA;
+	t_list* tabla_direcciones;
+
+} t_tabla_paginas;
+
+typedef struct{
+	int pos;
+} t_frame;
+
+typedef struct{
+	int pos;
+} t_frame_libre;
+
+typedef struct{
+	t_pagina_patota* pagina;
+} t_buffer_clock;
+
+typedef struct {
+
+	int tid;
+	int direccion_log;
+
+} t_direcciones_trips;
+
+int TAMANIO_MEMORIA;
+int32_t ESQUEMA_MEMORIA;
+int TAMANIO_PAGINA;
+int TAMANIO_SWAP;
+int32_t CRITERIO_SELECCION;
+char* PATH_SWAP;
+int32_t ALGORITMO_REEMPLAZO;
+char* IP;
+char* PUERTO;
+
+void* memoria_principal;
+
+//Paginacion
+
+void* memoria_virtual;
+t_dictionary* tabla_paginas_patota;
+t_list* frames_libres_principal;
+t_list* frames_swap;
+t_list* lista_para_reemplazo;
+int buffer_clock_pos;
+
+t_config* config;
+t_log* logger;
+
+typedef struct  __attribute__((__packed__)){
+
+	int pid;
+	int direccion_tareas;
+
+} t_pcb;
+
+typedef  struct  __attribute__((__packed__)) {
+
+	int tid;
+	char estado;
+	int posX;
+	int posY;
+	int proxima_instruccion;
+	int direccion_patota;
+
+} t_tcb;
+
+typedef struct{
+
+	int marco;
+	char* estado;
+	char* proceso;
+	char* pagina;
+
+} t_dump_pag;
+
+void init ();
+void iniciarMapa();
+void terminar ();
+int32_t get_esquema_memoria(char* esquema_config);
+int32_t get_algoritmo(char* algoritmo_config);
+void recibir_mensaje(int32_t* conexion);
+char get_status(t_status_code codigo);
+void hilo_servidor();
+void sig_handler(int n);
+char* get_timestamp();
+
+//Paginacion
+void configurar_paginacion();
+void crear_patota_paginacion(iniciar_patota_msg* mensaje, bool* status);
+void informar_movimiento_paginacion(informar_movimiento_ram_msg* mensaje, bool* status);
+void cambiar_estado_paginacion(cambio_estado_msg* mensaje, bool* status);
+char* siguiente_tarea_paginacion(solicitar_siguiente_tarea_msg* mensaje, bool* termino, bool* status);
+void expulsar_tripulante_paginacion(expulsar_tripulante_msg* mensaje, bool* status);
+t_list* get_frames_libres();
+t_list* get_frames_a_ubicar(int cantidad);
+void traer_pagina_a_memoria(t_pagina_patota* pagina);
+bool memoria_llena();
+bool swap_lleno();
+int32_t get_frame();
+t_list* swap_libres();
+int32_t get_frame_memoria_virtual();
+int32_t reemplazo_Clock();
+int32_t reemplazo_LRU();
+void guardar_en_memoria_principal(t_pagina_patota* pagina, void* datos);
+void guardar_en_memoria_swap(t_pagina_patota* pagina, void* datos);
+int generar_direccion_logica_paginacion(int pagina, int desplazamiento);
+void obtener_direccion_logica_paginacion(int* pagina, int* desplazamiento, int direccion);
+bool entra_en_swap(int cantidad);
+bool entra_en_memoria(int size_pcb);
+void leer_pagina_de_memoria(t_pagina_patota* pagina, void* to);
+void liberar_memoria_principal_paginacion(t_pagina_patota* pagina);
+void liberar_memoria_virtual(t_pagina_patota* pagina);
+int32_t paginas_necesarias(int offset, int size);
+void pasar_de_swap_a_principal(t_pagina_patota* pagina);
+void modificar_en_memoria_principal(t_pagina_patota* pagina, void* datos);
+void dump_paginacion(FILE* dump);
+void borrar_patota(t_tabla_paginas* tabla);
+
+/* Segmentacion */
+
+typedef struct{
+	int numero_segmento;
+	int inicio;
+	int tamanio;
+
+}segmento;
+
+typedef struct{
+	t_list* segmentos;
+	pthread_mutex_t  m_TABLA;
+
+}tabla_segmentos;
+
+typedef struct{
+	char* pid;
+	int numero_segmento;
+	int inicio;
+	int tamanio;
+
+}segmento_dump;
+
+//agrupa todas las tablas de segmentos
+t_dictionary* tablas_seg_patota;
+
+typedef enum{
+	FF = 0,
+	BF = 1
+} criterio_seleccion;
+
+//lista con todos los segmentos en memoria
+t_list* segmentos_en_memoria;
+
+//lista de segmentos libres
+t_list* segmentos_libres;
+
+int32_t get_criterio(char* cofig);
+void dump_segmentacion(FILE* dump);
+void agregar_seg_listas(segmento* segmento_nuevo);
+void ordenar_lista_segmentos_libres();
+void liberar_segmento(segmento* seg);
+bool entra_en_un_seg_libre(int size);
+int32_t get_espacio_libre(int size);
+bool entra_en_memoria_seg(int tamanio_necesario);
+
+void inicializar_segmentacion();
+
+void crear_patota_segmentacion(iniciar_patota_msg* mensaje, bool* status);
+void informar_movimiento_segmentacion(informar_movimiento_ram_msg* mensaje, bool* status);
+void cambiar_estado_segmentacion(cambio_estado_msg* mensaje, bool* status);
+char* siguiente_tarea_segmentacion(solicitar_siguiente_tarea_msg* mensaje, bool* termino, bool* status);
+void expulsar_tripulante_segmentacion(expulsar_tripulante_msg* mensaje, bool* status);
+segmento* buscar_segmento_tripulante(int id_tripulante, int id_patota);
+int encontrar_tripulante(int id_tripulante, int id_patota);
+int buscar_offset_tripulante(int id_tripulante, int id_patota);
+int obtener_limite(segmento* seg);
+void compactar_memoria(); //sin implementar
 
 #endif /* MI_RAM_HQ_H_ */
