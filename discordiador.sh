708f98881c4045acb387978d0182701032788f8b
snouthill <dagostino@est.frba.utn.edu.ar>
2021-07-20 21:01:37 -0300

    scripts para pruebas

--- discordiador.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-cd discordiador
-cd Debug
-rm valgrind.log
-valgrind --leak-check=full --log-file=valgrind.log ./discordiador
--- ram.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-cd mi_Ram_Hq
-cd Debug
-rm valgrind.log
-valgrind --leak-check=full --log-file=valgrind.log ./mi_Ram_Hq
